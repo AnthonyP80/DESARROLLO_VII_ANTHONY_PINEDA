94c255eb1763d1201465a458078aa5ad511331f6
AnthonyP80 <anthonii.p08@gmail.com>
2024-09-03 20:15:41 -0500

    Arrgelos de las funciones

--- PARCIALES/PARCIAL_1/funciones_tienda.php
+++ PARCIALES/PARCIAL_1/funciones_tienda.php
@@ -1,14 +1,30 @@
 <?php
 function calcular_descuentos($total_compra){
+    
+
+if ($total_compra < 100) {
+    return 0;
+
+} elseif ($total_compra >= 100 || $total_compra <=500 ) {
+    return $total_compra*0.5;
+} 
+elseif ($total_compra >= 501 || $total_compra <= 1000) {
+    return $total_compra*0.10;
+
+} else {
+    return $total_compra* 0.15;
+}
 
 }
 
+
 function aplicar_impuesto($subtotal){
+    return $subtotal *0.17;
 
 }
 
 function calcular_total($subtotal, $descuento, $impuesto){
-
+    return $subtotal- $descuento + $impuesto;
 }
 
 
