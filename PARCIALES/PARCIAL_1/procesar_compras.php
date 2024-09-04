4bc8e00d211aced40b7ba407a65a35440980885e
AnthonyP80 <anthonii.p08@gmail.com>
2024-09-03 20:16:26 -0500

    Procesos de los precios

---
+++ PARCIALES/PARCIAL_1/procesar_compras.php
@@ -0,0 +1,22 @@
+<?php
+include 'funciones_tienda.php';
+
+$productos = [
+'camisa' => 50,
+'pantalon'=> 70,
+'zapatos'=> 80,
+'calcetines'=> 10,
+'gorra'=> 25
+];
+
+$carrito = [
+    'camisa' => 2,
+    'pantalon'=> 1,
+    'zapatos'=> 1,
+    'calcetines'=> 3,
+    'gorra'=> 0
+    ];
+
+
+
+?>
